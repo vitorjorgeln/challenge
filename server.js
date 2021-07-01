929512cafeefec75832ba4edb57fddeeb5a8d100
Vitor Leal <vitorjorgeln@gmail.com>
2021-06-30 21:18:30 -0300

    colocando os arquivos da api no root para o Heroku reconhece-los

---
+++ server.js
@@ -0,0 +1,18 @@
+const axios = require ('axios')
+const express = require('express')
+const app = express()
+
+app.get('/getrepos', async (req, res) => {
+    try {
+        const {data} = await axios ('https://api.github.com/orgs/takenet/repos')
+        
+        let retorno = data.filter((dt) => dt.language === "C#");
+
+        return res.json(retorno)
+    } catch (error) {
+        return res.json(error)
+    }
+    
+})
+
+app.listen('666')
