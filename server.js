e6865a2ac421ed4760b420268aeb99961ca3b525
Vitor Leal <vitorjorgeln@gmail.com>
2021-06-30 22:12:17 -0300

    add metodo / server.js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const axios = require ('axios')
 const express = require('express')
 const app = express()
 
+app.get('/', async (req, res) => {
+    res.json({msg: "OK"})    
+})
+
+
 app.get('/getrepos', async (req, res) => {
     try {
         const {data} = await axios ('https://api.github.com/orgs/takenet/repos')
