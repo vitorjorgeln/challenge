516a47a877404bc87c300d5d077e24b2b7d74846
Vitor Leal <vitorjorgeln@gmail.com>
2021-07-01 23:59:45 -0300

    Refinando metodo de retorno + add readme

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Desafio
 Repositorio destinado ao desafio.
 
-Como executar
+Como executar:
 
     -> Clone o repositório
+    -> Instale o Node.js na versao estavel (por favor, versao *estavel*, ok?! 14.17 LTS)
     -> Instale as dependências com "npm i express nodemon cors axios"
+    -> Tambem precisamos do consign, logo, basta passar um "npm install consign"
     -> Inicie o servidor com "npm start" 
 
 A aplicação pode ser acessada em localhost:666 ou pela request GET: https://api-cs.herokuapp.com/getrepos
--- server.js
+++ server.js
@@ -6,34 +6,24 @@ const app = express()
 //criando rota
 app.get('/getrepos', async (req, res) => {
     try {
-        const {data} = await axios ('https://api.github.com/orgs/takenet/repos') // -> Pegando o retorno completo da api do git para os repositorios da take
-        
-        let retorno = data.filter((dt) => dt.language === "C#");// -> Filtrei os repositorios por liguagem. Isso me deu mais facilidade pra trabalhar com o retorno alem de me retornar o valores ja em ordem crescente. 
+        const {data} = await axios ('https://api.github.com/orgs/takenet/repos') //-> Pegando o retorno completo da api do git para os repositorios da take
 
-            //com certeza tinha um jeito melhor de pegar esses retornos, mas infelizmente por limitação tecnica, essa foi a forma que consegui :(
-            // tentei colocar um for dentro do forEach, e até consegui retornar os valores, mas tive dificuldade de montar um objeto parecido com o que eu consegui fazendo essa repeticao manualmente
-            retorno.forEach( value=> {
-                
-                let response = {
-                name1: retorno[0].full_name,
-                name2: retorno[1].full_name, 
-                name3: retorno[2].full_name,
-                name4: retorno[3].full_name,
-                name5: retorno[4].full_name,
-                descricao1: retorno[0].description,
-                descricao2: retorno[1].description,
-                descricao3: retorno[2].description,
-                descricao4: retorno[3].description,
-                descricao5: retorno[4].description
-                }
-                return res.json(response)
-                
-        }) 
-        
+        let retorno = data.filter((dt) => dt.language === "C#");// -> Filtrei os repositorios por liguagem. Isso me deu mais facilidade pra trabalhar com o retorno alem de me retornar o valores ja em ordem crescente. 
+            var response = {}
+
+            for (let i = 0; i < retorno.length; i++) {
+                Object.assign(response, {
+                    ['name'+i]: retorno[i].full_name,
+                    ['description'+i]: retorno[i].description
+                })
+            }
+
+            return res.json(response)
+
     } catch (error) {
         return res.json(error)
     }
-    
+
 })
 
 // me certificando que, quando em producao minha rota ouca a porta que o heroku deu a ela. Mas, quando eu testar em local, ela rodara na porta que setei. 
